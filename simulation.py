9260677ef69db378a3b1a15053a72ad6714f9e82
Rohit Sarathy <rohit@sarathy.org>
2019-01-06 10:30:38 -0800

    Add a base BankingSimulation class.
    The BankingSimulation class provides basic groundwork for setting up simple simulations demonstrating prize-based banking systems.

---
+++ simulation.py
@@ -0,0 +1,40 @@
+# A base class for simulating a bank that provides prize-based savings accounts.
+#
+# For the purposes of this simulation, we make the following assumptions:
+#
+# * A bank offers loans to its customers at an average of 8%.
+# * Wealth follows a standard Pareto distribution, where roughly 20% of the
+#   accounts will own 80% of the assets under management in our bank.
+# * All assets under management will be loaned out at a rate of 8%.
+
+import numpy as np
+
+
+# The assets under management (AUM) controlled by the bank.
+AUM = 10**6
+
+# The rates that the bank will offer loans at.
+LOAN_RATE = 0.08
+
+# The interest rate that the bank will offer to savings account by default.
+INTEREST_RATE = 0.03
+
+# The number of savings accounts under the bank.
+NUM_ACCOUNTS = 10000
+
+class BankingSimulation(object):
+  def __init__(self):
+    self.accounts = _make_accounts()
+    self.assets_under_management = np.sum(self.accounts)
+    self.profits = []
+    
+def _make_accounts():
+  # Seed all saving accounts with wealth obeying a standard Pareto distribution.
+  accounts = np.random.pareto(1, NUM_ACCOUNTS)
+  
+  # Scale each balance equally such that the assets under management is
+  # 1 million units.
+  temp_sum = np.sum(accounts)
+  for i in xrange(len(accounts)):
+    accounts[i] *= (AUM / temp_sum)
+  return accounts
---
+++ simulation_test.py
@@ -0,0 +1,11 @@
+from simulation import BankingSimulation
+import unittest
+
+class SimulationTest(unittest.TestCase):
+  def test_make_accounts(self):
+    """Checks that a simulation correctly populates the saving accounts."""
+    s = BankingSimulation()
+    self.assertAlmostEqual(s.assets_under_management, 1000000)
+    
+if __name__ == '__main__':
+  unittest.main()
